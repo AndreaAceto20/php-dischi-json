3905dedb3b7e175bc46e73d2afb11b1cad647b8e
Andrea Aceto <andrea.aceto20@gmail.com>
2025-05-01 12:46:52 +0200

    [add]formatter

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 
 require_once './functions.php'
 
 
 
-?>
+    ?>
 
 
 
@@ -14,29 +14,32 @@ require_once './functions.php'
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
     <title>Records</title>
 </head>
+
 <body>
     <div class="container">
-    
-        <?php 
-        foreach($records as $record){
+
+        <?php
+        foreach ($records as $record) {
             echo "<div class = card>$record[title] <br> 
             $record[author] <br>
             <img src=$record[cover] alt=$record[title]> <br>
             $record[date] <br>
             $record[genre]</div>";
         }
-    
-    
-    
-    ?>
-    <img src="" alt="">
-    
+
+
+
+        ?>
+        <img src="" alt="">
+
     </div>
 </body>
+
 </html>
